9f75562fbcac09b16610a2d188d5627e62772a3a
Fernanda Herrera <fherrera@tuxpan.com>
2025-09-29 16:46:38 -0300

    se elimina llave extra y se corrige error de compilación en OsticketServiceImpl"

--- src/main/java/com/tuxpan/soportesw/osreporte_backend/osticket/services/OsticketServiceImpl.java
+++ src/main/java/com/tuxpan/soportesw/osreporte_backend/osticket/services/OsticketServiceImpl.java
@@ -93,6 +93,5 @@ public List<TicketsPorEstadoDTO> obtenerTicketsPorEstado(String fechaInicio, Str
                 }
             }
             return tipoActividad;
-        }
-        }
+    }
 }
